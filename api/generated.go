2dd15a5173a25b060b602f4f61e9cd768d89f5f8
Rohit Raut <rohitraut3366@gmail.com>
2025-02-17 10:11:57 +0530

    remove conflicts

--- api/generated.go
+++ api/generated.go
@@ -1944,11 +1944,7 @@ const GetScheduledJobs_Operation = `
 query GetScheduledJobs($slug: ID!, $agentQueryRules: [String!], $after: String) {
   organization(slug: $slug) {
     id
-<<<<<<< Updated upstream
-    jobs(state: [SCHEDULED], type: [COMMAND], first: 100, after: $after, agentQueryRules: $agentQueryRules) {
-=======
     jobs(state: [SCHEDULED], type: [COMMAND], first: 500, after: $after, agentQueryRules: $agentQueryRules) {
->>>>>>> Stashed changes
       count
       edges {
         node {
@@ -1992,11 +1988,7 @@ func GetScheduledJobs(
 		Variables: &map[string]interface{}{
 			"slug":            slug,
 			"agentQueryRules": agentQueryRules,
-<<<<<<< Updated upstream
-			"first":           100,  // Fetch up to 100 jobs per page
-=======
 			"first":           500,  // Fetch up to 100 jobs per page
->>>>>>> Stashed changes
 		},
 	}
 
@@ -2039,11 +2031,7 @@ func GetScheduledJobs(
 			Variables: &map[string]interface{}{
 				"slug":            slug,
 				"agentQueryRules": agentQueryRules,
-<<<<<<< Updated upstream
-				"first":           100,  // Fetch up to 100 jobs per page
-=======
 				"first":           500,  // Fetch up to 100 jobs per page
->>>>>>> Stashed changes
 				"after":           *cursor, // Pass the cursor for pagination
 			},
 		}
